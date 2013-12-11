1b495eb9762542b855a7a3947fe1de2deac6d74d
Shayde <sln7755@rit.edu>
2013-12-10 19:36:27 -0500

    Problem 2 implementation

---
+++ C#/Problem2/Problem2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int prev = 1;
+            int curr = 1;
+            int sum = 0;
+            int next = 2;
+
+            while (curr < 4000000)
+            {
+                if ((curr % 2) == 0)
+                {
+                    sum += curr;
+                }
+                prev = curr;
+                curr = next;
+                next = prev + curr;
+            }
+            Console.Write("Sum of even ints in Fib Seq: {0}", sum);
+            Console.ReadKey();
+        }
+    }
+}
